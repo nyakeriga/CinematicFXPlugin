856537dcc71f50af5728d1cc86c406d92b4fa366
ISABOKE <isabokenockgmail.com>
2025-12-23 12:11:19 -0700

    Add minimal PiPL.r resource file for macOS plugin compatibility

---
+++ src/resources/PiPL.r
@@ -0,0 +1,12 @@
+resource 'PiPL' (10000) {
+    {
+        'PiPL',
+        {
+            'Name', "CinematicFX",
+            'Category', "Stylize",
+            'MatchName', "com.cinebloom.cinematicfx",
+            'Type', 'AEfx',
+            'AEEffect', {}
+        }
+    }
+};
